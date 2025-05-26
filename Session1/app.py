5ea34f365c72eab84adafe8f88f639b52ba1bb18
jim2107054 <mdjahid205hasan@gmail.com>
2025-05-26 09:29:13 +0600

    session one done

--- Session1/app.py
+++ Session1/app.py
@@ -17,15 +17,18 @@ def About():
 #path parameters
 @app.route('/user/<name>') #after the /user path, we are expecting a name from the user
 def user(name):
-    return f"<h1>Hello, {name}!</h1><p>Welcome to your user page.</p>"
+    return f"<h1>Hello, {name.title()}!</h1><p>Welcome to your user page.</p>"
 
 @app.route('/user/<name>/<int:age>') #we can also specify the type of the parameter
 def user_with_age(name, age):
     return f"<h1>Hello, {name}!</h1><p>You are {age} years old.</p>"
 
+@app.route('/user/<name>/<int:age>/<float:height>') #we can also specify multiple parameters
+def user_with_age_and_height(name, age, height):
+    return f"<h1>Hello, {name.title()}!</h1><p>You are {age} years old and {height} feets tall.</p>"
 
 
 #state the application
 # to run the app, use the command: flask run
-if __name__ == '__main__': # This application will run only if we are in app.py module
+if __name__ == '__main__': # This application will run only if we are in app.py module. To avoid any import issues, we use this condition.
     app.run(debug=True,port=5000)
